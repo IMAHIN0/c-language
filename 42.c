eaa513e34c70c2f7da8d230b6f282314ad8a0948
Jet Im <imj835855@gmail.com>
2023-04-06 05:34:18 +0000

    연습

---
+++ 42.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int num1=16;
+   int num2;
+   int sum=0;	
+   num2 = 61;
+   sum = num1 + num2;
+	printf("%d + %d = %d" ,num1 ,num2 ,sum);
+	
+	
+	
+   return 0;
+}
