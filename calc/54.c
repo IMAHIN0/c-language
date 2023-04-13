dd38ccedd959b5001dd3d7f311ad293c5a2681d6
Jet Im <imj835855@gmail.com>
2023-04-13 06:24:11 +0000

    연습

---
+++ calc/54.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int main() {
+
+	int a, a1, 1a;
+	int A, cat, Cat, cAt, caT; 
+	int _a, __a, ___;
+	int if;
+	
+	return 0;
+} 
---
+++ calc/55.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+
+	int a =3;
+	a = a + 6;
+
+	
+ 	if(a>5) { 
+		printf("%d\n", a);
+	}else {
+		printf("%d\n", a-2);
+	}
+	return 0;
+}	
